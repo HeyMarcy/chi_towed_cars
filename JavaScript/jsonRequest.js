620484346365fc775a8ea15c5ef11f61e4a9f5ee
Amanda J Kendal-Brown <ajkendal@neiu.edu>
2017-09-24 10:04:03 -0500

    finished for second selection

--- JavaScript/jsonRequest.js
+++ JavaScript/jsonRequest.js
@@ -7,14 +7,15 @@ request.send();
 
 function returnCar(searchState, searchPlate, searchMake, searchColor) {
     "use strict";
+    document.getElementById("returnedCars").innerHTML = "";
     var towedCar = request.response;
     var length = Object.keys(towedCar).length;
-    searchColor.trim();
-    searchColor.trim();
-    searchPlate.toUpperCase();
-    searchState.trim();
     
-    console.log("returnCar function" + searchColor + searchMake + searchPlate + searchState);
+    searchColor = searchColor.trim();
+    searchPlate.toUpperCase();
+    searchPlate = searchPlate.trim();
+    searchState = searchState.trim();
+    searchMake = searchMake.trim();
     
     var carArray = towedCar.filter(function (cars) {
         return ((cars.state === searchState && cars.plate === searchPlate) || (cars.color === searchColor && cars.make === searchMake));
@@ -22,14 +23,16 @@ function returnCar(searchState, searchPlate, searchMake, searchColor) {
     
     if (carArray.length >= 1) {
         for (var x = 0; x < carArray.length; x++){
-            var date = new Date(carArray[x].tow_date);
+            
+            var towDate = new Date(carArray[x].tow_date);
+            
             var address = carArray[x].towed_to_address;
             address = address.replace(/\s/g, "+");
             var googleLink = ("https://www.google.com/maps/place/" + address + ",Chicago,+IL");
             
             document.getElementById("returnedCars").innerHTML += ("<h3>Located Car</h3>" + "<p>" + "<b>Plate: </b>" + carArray[x].plate + " <b>State: </b>" + carArray[x].state + "<br />" + "<b>Make: </b>" + carArray[x].make + " <b>Color: </b>" + carArray[x].color + "<br /></p>");   
             
-            document.getElementById("returnedCars").innerHTML += ("<h3>Tow Location</h3>" + "<b>Inventory No. </b>" + carArray[x].inventory_number + "<br />" + "<b>Address: </b>" + "<a target='_blank' href="  + googleLink + ">" +  carArray[x].towed_to_address + "</a>" + "<br />" + "<b>Phone: </b>" + carArray[x].tow_facility_phone + "</p>");                                                 
+            document.getElementById("returnedCars").innerHTML += ("<h3>Tow Location</h3>" + "<p>" + "<b>Tow Date: </b>" + towDate.toDateString() + "<br />" + "<b>Inventory No. </b>" + carArray[x].inventory_number + "<br />" + "<b>Address: </b>" + "<a target='_blank' href="  + googleLink + ">" +  carArray[x].towed_to_address + "</a>" + "<br />" + "<b>Phone: </b>" + carArray[x].tow_facility_phone + "</p>");                                                 
         }  
     }
     else {
@@ -46,9 +49,6 @@ document.getElementById('plateStateForm').addEventListener('submit', (evt) => {
     returnCar(state, plate, "", "");
     console.log("HERE" + state + " " + plate);
     evt.preventDefault(); 
-    
-    //return("IL", "V444887", "", "");
-    //returnCar("", "", "CHEV", "GLD");
 })
 
 document.getElementById('colorMakeForm').addEventListener('submit', (evt) => {
